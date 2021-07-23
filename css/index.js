767c1e515d6a367f19e70e9ec0c72ca4d14e911c
wildlb <wildlb@hotmail.com>
2021-07-23 17:54:01 -0300

    EJS

--- css/index.js
+++
@@ -1,8 +0,0 @@
-const express = require ('express');
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('index.js')
-});
-
-app.listen(process.env.PORT || 3000);
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('index.js')
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('servidor rodando!!')
+});
